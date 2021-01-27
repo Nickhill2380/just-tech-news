95e42385f4b92efd2bb850054d5c520a2e395592
nickhill2380 <nickhill2380@gmail.com>
2021-01-26 20:02:38 -0600

    password security

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,28 +47,29 @@ router.post('/', (req, res) => {
     });
 });
 
-router.post('/login', (req,res) => {
-  // expects email
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email
     }
-   }).then(dbUserData => {
-    if(!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!'});
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
       return;
-     }
-
+    }
+    
     const validPassword = dbUserData.checkPassword(req.body.password);
 
-    if(!validPassword) {
-      res.status(400).json({ message: 'Incorrect password'})
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
 
-    res.json({ user:dbUserData, message: 'You on now logged in!'})
-  })
-})
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
+
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
