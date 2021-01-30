51027260f6388764e7e055960c33c14f4961d1f0
nickhill2380 <nickhill2380@gmail.com>
2021-01-30 17:23:08 -0600

    changes to routes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,62 +1,46 @@
 const router = require('express').Router();
-const { Comment, User, Post } = require('../../models');
+const { Comment } = require('../../models');
 
-router.get('/', (req,res) => {
-    Comment.findAll({
-        attributes: [
-            'id',
-            'comment_text',
-            'created_at'
-        ],
-        order: [['created_at','DESC']],
-        include: [
-            {
-                model: User,
-                attributes:['username']
-            },
-            {
-                model: Post,
-                attributes:['title']
-            }
-        ]
-    })
+router.get('/', (req, res) => {
+  Comment.findAll()
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
-        console.log(err)
-        res.status(400).json(err)
-    })
-
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req,res) => {
-    Comment.create({
-    comment_text:req.body.comment_text,
+router.post('/', (req, res) => {
+  // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
+  Comment.create({
+    comment_text: req.body.comment_text,
     user_id: req.body.user_id,
     post_id: req.body.post_id
-    })
+  })
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    })
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
-router.delete('/:id', (req,res) => {
-    Comment.destroy({
-        where:{
-        id: req.params.id
+router.delete('/:id', (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id
     }
-    }) .then(dbCommentData => {
-        if(!dbCommentData) {
-            res.status(404).json({message: 'No comment with that id was found.'});
-            return;
-        }
-        res.json(dbCommentData);
+  })
+    .then(dbCommentData => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'No comment found with this id!' });
+        return;
+      }
+      res.json(dbCommentData);
     })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote, Comment } = require('../../models');
+const { Post, User, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -22,6 +22,10 @@ router.get('/', (req, res) => {
           model: User,
           attributes: ['username']
         }
+      },
+      {
+        model: User,
+        attributes: ['username']
       }
     ]
   })
@@ -48,8 +52,14 @@ router.get('/:id', (req, res) => {
       {
         model: Comment,
         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {   model: User,
-            attributes: ['username']}
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
       }
     ]
   })
@@ -82,11 +92,11 @@ router.post('/', (req, res) => {
 
 router.put('/upvote', (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
+  Post.upvote(req.body, { Vote, Comment, User })
+    .then(updatedVoteData => res.json(updatedVoteData))
     .catch(err => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(500).json(err);
     });
 });
 
